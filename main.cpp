f38671ae9823171a2778e26ef9605333fc0abe36
Justin Gries <justin.gries@gmail.com>
2021-10-25 14:19:13 -0700

    Initial checkin of boilerplate project files

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+/*
+ * Course: Chaffey College 2021-Fall CS2
+ * Prof: Dr. Tracy Kocher
+ * Date:
+ * Project:
+ * Coder: Justin Gries
+ * Tester(s):
+ *
+ * Prompt: Implement a base class Appointment and derived classes Onetime, Daily, Weekly, and Monthly. An appointment
+ * has a description (for example, “see the dentist”) and a date and time. Write a virtual function occurs_on(int
+ * year, int month, int day) that checks whether the appointment occurs on that date. For example, for a monthly
+ * appointment, you must check whether the day of the month matches. Then fill a vector of Appointment* with a
+ * mixture of appointments. Have the user enter a date and print out all appointments that happen on that date.
+ */
+int main(int argc, char **argv) {
+    std::cout << "Hello 9.09" << std::endl;
+    return 0;
+}
